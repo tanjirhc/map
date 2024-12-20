f1aaac4c935e9776982d5a9427c769691bf60308
Tanjir Hasan <130759310+tanjirhc@users.noreply.github.com>
2024-12-20 18:57:16 +0600

    map method as cubes

--- index.js
+++ index.js
@@ -2,10 +2,14 @@
 
 const numbers = [1, 2, 3, 4, 5];
 const squares = numbers.map(square);
+const cubes = numbers.map(cube);
 
-console.log(squares);
+console.log(cubes);
 
 function square(element){
   return Math.pow(element, 2)
+}
 
+function cube(element){
+  return Math.pow(element, 3)
 }
