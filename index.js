7a82bd407a1f882f498cb46c8720c1b6893f14f0
Tanjir Hasan <130759310+tanjirhc@users.noreply.github.com>
2024-12-20 20:41:58 +0600

    lowerCase

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 // .map() = accepts a callback and applies that function to each of an array, then return a new array
 
 const students = ["Tanjir", "Mohsin", "Rabbi", "Kabir"];
-const studentsUpper = students.map(upperCase);
+const studentsUpper = students.map(lowerCase);
 
 console.log(studentsUpper);
 
 function upperCase(element){
   return element.toUpperCase();
+}
+
+function lowerCase(element){
+  return element.toLowerCase();
 }
